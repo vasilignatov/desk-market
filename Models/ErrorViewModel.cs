baaf56d32992c11596035caf4c721770dbfbb67d
Vasil Ignatov <vasilignatow@gmail.com>
2024-12-18 12:53:59 +0200

    add

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DeskMarket.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
